8030a1e4d057b6c86c38e33e3425edb1772b4641
harry48225 <harrybest800@gmail.com>
2022-07-05 22:46:17 +0100

    Add action buttons to settings

--- app/octoscale-app/app/Settings.svelte
+++ app/octoscale-app/app/Settings.svelte
@@ -10,18 +10,31 @@ import Home from "./Home.svelte";
   <actionBar title="Settings">
     <actionItem text="home" on:tap={() => {navigate({page: Home})}}/>
   </actionBar>
-  <stackLayout>
-    <label>Developer settings</label>
-    <SliderSetting name="min cut-off"/>
-    <SliderSetting name="D cut-off"/>
-    <SliderSetting name="beta"/>
-    <SliderSetting name="settled tolerance"/>
-    <SliderSetting name="unsettled tolerance"/>
-  </stackLayout>
+  <flexboxLayout flexDirection="column">
+    <stackLayout>
+      <label>Developer settings</label>
+      <SliderSetting name="min cut-off"/>
+      <SliderSetting name="D cut-off"/>
+      <SliderSetting name="beta"/>
+      <SliderSetting name="settled tolerance"/>
+      <SliderSetting name="unsettled tolerance"/>
+    </stackLayout>
+
+    <flexboxLayout justifyContent="space-around">
+      <button>query scale</button>
+      <button>apply</button>
+    </flexboxLayout>
+  </flexboxLayout>
 </page>
 
 <style lang="scss">
   Label {
     font-size: 16px;
+  }
+
+  FlexboxLayout {
+    StackLayout {
+      flex-grow: 1;
+    }
   }
 </style>
