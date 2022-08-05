7d039f228e4ff7eb1e3ec2b0056d454986bd50d2
harry48225 <harrybest800@gmail.com>
2022-08-05 19:36:27 +0100

    Create number field

---
+++ app/octoscale-app/app/components/NumberField.svelte
@@ -0,0 +1,27 @@
+<script lang="ts" context="module">
+  export type ReturnPressEvent = CustomEvent<{ object: { text: string; }; }>;
+</script>
+
+<script lang="ts">
+  // @ts-nocheck
+  // Disable error checking to disable errors for the on:returnPress
+  // a bit of a hack...
+  import { createEventDispatcher } from "svelte";
+  export let value: number; 
+  const dispatch = createEventDispatcher();
+
+  
+
+  const returnPress = (e: ReturnPressEvent) => {
+    dispatch('returnPress', e)
+  };
+</script>
+
+<textField text={value.toString()} 
+  returnKeyType="done" 
+  closeOnReturn="true" 
+  keyboardType="number" 
+  on:returnPress={returnPress}
+>
+
+</textField>
