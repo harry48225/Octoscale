7987de623175c0e62257d5b1bf59667f9e5151b1
harry48225 <harrybest800@gmail.com>
2022-08-07 21:41:52 +0100

    Style graph

--- app/octoscale-app/app/components/Graph.svelte
+++ app/octoscale-app/app/components/Graph.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import { Canvas } from '@nativescript/canvas'
+  import { Canvas } from '@nativescript/canvas';
 
   type Point = {x: number, y: number}
   export const data: Point[] = [{x: 0, y: 0}, {x: 10, y: 10}, {x: 20, y: 30}, {x: 30, y: 35}];
 
-  const drawFilledRegion = (canvas: Canvas, ctx: CanvasRenderingContext2D) => {
+  const drawFilledRegion = (canvas: Canvas, ctx: CanvasRenderingContext2D, data: Point[]) => {
     ctx.fillStyle = '#E4E6C3';
     ctx.beginPath();
     ctx.moveTo(data[0].x, data[0].y);
@@ -19,8 +19,19 @@
     ctx.fill();
   };
 
-  const drawGraphLine = (canvas: Canvas, ctx: CanvasRenderingContext2D) => {
+  const scaleData = (data: Point[], canvasHeight: number, canvasWidth: number, filledProportionX: number = 0.8, filledProportionY: number = 0.8): Point[] => {
+    const maxY = Math.max(...data.map(p => p.y));
+    const maxX = Math.max(...data.map(p => p.x));
+
+    const yScale = (canvasHeight/maxY) * filledProportionY;
+    const xScale = (canvasWidth/maxX) * filledProportionX;
+
+    return data.map(p => ({x: p.x * xScale, y: p.y * yScale}));
+  }
+
+  const drawGraphLine = (canvas: Canvas, ctx: CanvasRenderingContext2D, data: Point[]) => {
     ctx.strokeStyle = '#899878';
+    ctx.lineWidth = 8;
 
     ctx.beginPath();
     ctx.moveTo(data[0].x, data[0].y);
@@ -31,11 +42,11 @@
     ctx.stroke();
   };
 
-  const drawGraphEndMarker = (canvas: Canvas, ctx: CanvasRenderingContext2D) => {
+  const drawGraphEndMarker = (canvas: Canvas, ctx: CanvasRenderingContext2D, data: Point[]) => {
     const lastPoint = data[data.length - 1];
     //ctx.moveTo(data[lastIndex].x, data[lastIndex].y);
     ctx.beginPath();
-    ctx.arc(lastPoint.x, lastPoint.y, 1, 0, 2*Math.PI);
+    ctx.arc(lastPoint.x, lastPoint.y, 16, 0, 2*Math.PI);
     ctx.fillStyle = '#899878';
     ctx.fill();
   };
@@ -48,15 +59,27 @@
     if (!(ctx instanceof CanvasRenderingContext2D)) return
     
     ctx.transform(1, 0, 0, -1, 0, canvas.height as number)
-    ctx.scale(20, 20);
 
-    drawFilledRegion(canvas, ctx);
-    drawGraphLine(canvas, ctx);
-    drawGraphEndMarker(canvas, ctx);
+    const scaledData = scaleData(data, canvas.height, canvas.width);
+
+    drawFilledRegion(canvas, ctx, scaledData);
+    drawGraphLine(canvas, ctx, scaledData);
+    drawGraphEndMarker(canvas, ctx, scaledData);
   }
 </script>
 
-<canvas id="canvas" width="100%" height="100%" on:ready={canvasReady}/>
+<flexboxLayout>
+  <canvas id="canvas" width="100%" height="100%" on:ready={canvasReady}/>
+</flexboxLayout>
 
-<style>
+<style lang="scss">
+  FlexboxLayout {
+    border-color: #899878;
+    border-width: 8px;
+    border-radius: 64px;
+    margin: 32px;
+    overflow: hidden;
+    padding-left: 64px;
+    padding-right: 64px;
+  }
 </style>
