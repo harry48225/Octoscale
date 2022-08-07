fe67e8430763daf2432aa14c4ed69519f43708a6
harry48225 <harrybest800@gmail.com>
2022-08-07 15:23:22 +0100

    Convert home to 2 spcaes

--- app/octoscale-app/app/Home.svelte
+++ app/octoscale-app/app/Home.svelte
@@ -1,48 +1,46 @@
 <script lang="ts">
-    import MassDisplay from './components/MassDisplay.svelte';
-    import Timer from './components/Timer.svelte';
-    import { onMount } from 'svelte';
-    import { connectToScale, displayedMass, tare, isTiming, timerDurationSeconds } from  './bleManager';
-    import ActionButtons from './components/ActionButtons.svelte';
-    import ConnectionBanner from './components/ConnectionBanner.svelte';
-    import Graph from './components/Graph.svelte';
-    import { navigate } from 'svelte-native';
-    import Settings from './Settings.svelte';
-    import BottomNavigationBar from './components/BottomNavigationBar.svelte';
+  import MassDisplay from './components/MassDisplay.svelte';
+  import Timer from './components/Timer.svelte';
+  import { onMount } from 'svelte';
+  import { connectToScale, displayedMass, tare, isTiming, timerDurationSeconds } from  './bleManager';
+  import ActionButtons from './components/ActionButtons.svelte';
+  import ConnectionBanner from './components/ConnectionBanner.svelte';
+  import Graph from './components/Graph.svelte';
+  import { navigate } from 'svelte-native';
+  import Settings from './Settings.svelte';
+  import BottomNavigationBar from './components/BottomNavigationBar.svelte';
         
-    let mass: number;
-    displayedMass.subscribe(val => mass = val);
+  let mass: number;
+  displayedMass.subscribe(val => mass = val);
 
-    let timing: boolean;
-    isTiming.subscribe(val => timing = !!val);
+  let timing: boolean;
+  isTiming.subscribe(val => timing = !!val);
 
-    let duration: number;
-    timerDurationSeconds.subscribe(val => duration = val);
+  let duration: number;
+  timerDurationSeconds.subscribe(val => duration = val);
 
-    onMount(async () => {
-        await connectToScale();
-    })
+  onMount(async () => {
+      await connectToScale();
+  });
 </script>
 
-
 <page>
-    <actionBar title="octoscale">
-    </actionBar>
-    <flexboxLayout>
-        <ConnectionBanner/>
-        <MassDisplay mass={mass}/>
-        <Timer isTiming={timing} durationSeconds={duration}/>
-        <Graph/>
-        <ActionButtons tareCallback={tare}/>
-        <BottomNavigationBar/>
-    </flexboxLayout>
+  <actionBar title="octoscale">
+  </actionBar>
+  <flexboxLayout>
+    <ConnectionBanner/>
+    <MassDisplay mass={mass}/>
+    <Timer isTiming={timing} durationSeconds={duration}/>
+    <Graph/>
+    <ActionButtons tareCallback={tare}/>
+    <BottomNavigationBar/>
+  </flexboxLayout>
 </page>
 
-
 <style lang="scss">
-    FlexboxLayout {
-        flex-direction: column;
-        justify-items: center;
-        justify-content: space-between;
-    }
+  FlexboxLayout {
+    flex-direction: column;
+    justify-items: center;
+    justify-content: space-between;
+  }
 </style>
