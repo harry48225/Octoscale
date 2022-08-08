dd3aed4ef7eed292cf7c3893225f9e16cb090843
harry48225 <harrybest800@gmail.com>
2022-08-08 22:39:34 +0100

    Add ticks

--- app/octoscale-app/app/components/Graph.svelte
+++ app/octoscale-app/app/components/Graph.svelte
@@ -2,7 +2,7 @@
   import { Canvas } from '@nativescript/canvas';
 
   type Point = {x: number, y: number}
-  export const data: Point[] = [{x: 0, y: 0}, {x: 10, y: 10}, {x: 20, y: 30}, {x: 30, y: 35}, {x: 50, y: 38}];
+  export const data: Point[] = [{x: 0, y: 0}, {x: 10, y: 10}, {x: 20, y: 30}, {x: 30, y: 35}, {x: 50, y: 38}, {x: 53, y: 39}];
 
   const drawFilledRegion = (canvas: Canvas, ctx: CanvasRenderingContext2D, data: Point[]) => {
     ctx.fillStyle = '#E4E6C3';
@@ -19,14 +19,14 @@
     ctx.fill();
   };
 
-  const scaleData = (data: Point[], canvasHeight: number, canvasWidth: number, filledProportionX: number = 0.8, filledProportionY: number = 0.8): Point[] => {
+  const getDataScale = (canvasHeight: number, canvasWidth: number, filledProportionX: number = 0.8, filledProportionY: number = 0.8) => {
     const maxY = Math.max(...data.map(p => p.y));
     const maxX = Math.max(...data.map(p => p.x));
 
     const yScale = (canvasHeight/maxY) * filledProportionY;
     const xScale = (canvasWidth/maxX) * filledProportionX;
 
-    return data.map(p => ({x: p.x * xScale, y: p.y * yScale}));
+    return [xScale, yScale];
   }
 
   const drawGraphLine = (canvas: Canvas, ctx: CanvasRenderingContext2D, data: Point[]) => {
@@ -60,11 +60,36 @@
     
     ctx.transform(1, 0, 0, -1, 0, canvas.height as number)
 
-    const scaledData = scaleData(data, canvas.height, canvas.width);
+    const [xScale, yScale] = getDataScale(canvas.height, canvas.width);
 
+    const scaledData = data.map(p => ({x: p.x * xScale, y: p.y * yScale}));
+
+    drawHorizontalTicks(canvas, ctx, xScale);
     drawFilledRegion(canvas, ctx, scaledData);
     drawGraphLine(canvas, ctx, scaledData);
     drawGraphEndMarker(canvas, ctx, scaledData);
+  }
+
+  const drawHorizontalTicks = (canvas: Canvas, ctx: CanvasRenderingContext2D, xScale: number) => {
+    const tickWidth = 4;
+    ctx.strokeStyle = '#899878';
+    ctx.lineWidth = tickWidth;
+    ctx.font = '60px Arial';
+    for (let xCoordinate = tickWidth/2; xCoordinate < (canvas.width as number) - tickWidth/2; xCoordinate += 5*xScale) {
+      ctx.beginPath();
+      ctx.moveTo(xCoordinate, 0);
+      ctx.lineTo(xCoordinate, (canvas.height as number) * 0.9);
+      ctx.stroke();
+
+      // if (xCoordinate < (canvas.width as number) - 200) {
+      //   console.log(`drawing tick at ${xCoordinate}, ${Math.floor((xCoordinate - tickWidth / 2)/xScale)}s`);
+      //   ctx.transform(1, 0, 0, -1, 0, canvas.height as number);
+      //   ctx.fillText(`s`, xCoordinate, 200);
+      //   ctx.transform(1, 0, 0, -1, 0, canvas.height as number);
+      // }
+    }
+
+    console.log('done');
   }
 </script>
 
