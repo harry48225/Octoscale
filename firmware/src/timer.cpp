544e4e264164e79029b4ee662e424bfc44732058
Harry Best <harrybest800@gmail.com>
2022-09-10 12:35:17 +0100

    add startWithCountdown

--- firmware/src/timer.cpp
+++ firmware/src/timer.cpp
@@ -1,11 +1,30 @@
 #include <Arduino.h>
 #include "timer.h"
 #include "bluetooth.h"
+#include "display.h"
+#include "speaker.h"
 
 namespace Timer {
   bool timing = false;
   unsigned long startTime = 0;
   unsigned long duration = 0;
+
+  void startWithCountDown(int countdownDurationSeconds) {
+    int secondsRemaining = countdownDurationSeconds;
+
+    while (secondsRemaining > 0) {
+      Display::showCountdown(secondsRemaining);
+      Speaker::beep(
+        200 + 200 * ((countdownDurationSeconds - secondsRemaining))/countdownDurationSeconds,
+        200
+      );
+      delay(800);
+      secondsRemaining--;    
+    }
+
+    Speaker::beep(1000, 400);
+    start();
+  }
 
   void start() {
     startTime = millis();
--- firmware/src/timer.h
+++ firmware/src/timer.h
@@ -1,6 +1,7 @@
 namespace Timer {
   void stop();
   void start();
+  void startWithCountDown(int countdownDurationSeconds);
 
   unsigned long getSecondsElapsed();
 }
