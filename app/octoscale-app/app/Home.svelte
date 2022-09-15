c559f10746a162c6315303807a3574ea869547f2
Harry Best <harrybest800@gmail.com>
2022-09-15 11:12:27 +0100

    small tweaks

--- app/octoscale-app/app/Home.svelte
+++ app/octoscale-app/app/Home.svelte
@@ -2,12 +2,10 @@
   import MassDisplay from './components/MassDisplay.svelte';
   import Timer from './components/Timer.svelte';
   import { onMount } from 'svelte';
-  import { connectToScale, displayedMass, tare, isTiming, timerDurationSeconds, aButton, bButton } from  './bleManager';
+  import { connectToScale, displayedMass, isTiming, timerDurationSeconds, aButton, bButton } from  './bleManager';
   import ActionButtons from './components/ActionButtons.svelte';
   import ConnectionBanner from './components/ConnectionBanner.svelte';
   import Graph from './components/Graph.svelte';
-  import { navigate } from 'svelte-native';
-  import Settings from './Settings.svelte';
   import BottomNavigationBar from './components/BottomNavigationBar.svelte';
         
   let mass: number;
@@ -33,7 +31,7 @@
     <Timer isTiming={timing} durationSeconds={duration}/>
     <Graph/>
     <ActionButtons>
-      <button on:tap="{aButton}" text="time" />
+      <button on:tap="{aButton}" text="time"/>
       <button on:tap="{bButton}" text="tare"/>
     </ActionButtons>
     <BottomNavigationBar/>
